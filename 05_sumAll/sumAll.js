a4514cdde2e2885386cd6cecf9bf1e10d833a120
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-24 16:25:19 -0400

    Create function to add all numbers between two arg

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (typeof min !== "number" || typeof max !== "number") {
+        return "ERROR"
+    } else if (min < 0 || max < 0) {
+        return "ERROR"
+    } else {
+        let sum = 0
+        for (let i = Math.min(min, max); i <= Math.max(min, max); i++) {
+            sum += i
+        }
 
+        return sum
+    }
 };
 
 // Do not edit below this line
