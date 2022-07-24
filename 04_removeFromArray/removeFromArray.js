25a062746d0e276bc2a22916a5667496434be955
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-24 16:16:46 -0400

    Create function to remove values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    let newArray = []
+    let test = true
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (array[i] === args[j]) {
+                test = false
+            }
+        }
+        (test) ? newArray.push(array[i]) : test = true
+    }
+    return newArray
 };
 
 // Do not edit below this line
