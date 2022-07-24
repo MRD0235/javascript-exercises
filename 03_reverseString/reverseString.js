8ac1dc11baa50cc9737ed44568a95a62c06b68ab
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-24 15:42:44 -0400

    Create function to reverse letters in a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // initialize empty string for original to be copied into
+    newString = ""
+    // take last letter of original and put it in first slot
+    for (let i = string.length; i >= 0; i--) {
+        let letter = string.slice(i-1,i)
+        newString += letter
+    }
+    return newString
 };
 
 // Do not edit below this line
