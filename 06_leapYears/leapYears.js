d49bd87024d05a79838ff2d0802e17c132c3057f
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-24 16:32:57 -0400

    Create function to determine leap years from given

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+    if ((testYear % 4 === 0 && testYear % 100 !== 0) || (testYear % 100 === 0 && testYear % 400 === 0)) {
+        return true
+    } else if (testYear % 100 === 0 && testYear % 400 !== 0) {
+        return false
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
