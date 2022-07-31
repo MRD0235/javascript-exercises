a382d115a107587df9ef2b274e0030b953f8a948
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-31 14:34:14 -0400

    Create math functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(numSet) {
+  let total = 0
+  for (let i = 0; i < numSet.length; i++) {
+    total += numSet[i]
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(numSet) {
+  let total = 1
+  for (let i = 0; i < numSet.length; i++) {
+    total *= numSet[i]
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let total = 1
+  for (let i = number; i > 0; i--) {
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
