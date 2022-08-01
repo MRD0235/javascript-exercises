3119d7a09504f267251c6377169ac052c8699c33
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-31 22:32:23 -0400

    Create function to return given fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(desiredFibNum) {
+    desiredFibNum = Math.floor(desiredFibNum)
+    if (desiredFibNum <= 0) {return "OOPS"}
+    else {    
+        let oneBack = 1
+        let twoBack = 1
+        if (desiredFibNum === 1 || desiredFibNum === 2) return 1
+        for (let i = 2; i < desiredFibNum; i++) {
+            fibNum = oneBack + twoBack
+            twoBack = oneBack
+            oneBack = fibNum
+        }
+        return fibNum
+    }
+    return desiredFibNum
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
