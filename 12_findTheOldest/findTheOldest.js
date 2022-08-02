cb3b92492cf8733c67f1c57ca64631132bb430d6
Matt Dorchak <matthewdorchak235@gmail.com>
2022-08-01 23:47:15 -0400

    Create function to determine oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleBirthYear = people.map(person => person.yearOfBirth)  
+    let peopleDeathYear = people.map(person2 => person2.yearOfDeath)
+    
+    let oldestAge = 0
 
+    for (let i = 0; i < peopleBirthYear.length; i++) {
+        if (peopleDeathYear[i] === undefined) {peopleDeathYear[i] = 2022}
+        let age = peopleDeathYear[i] - peopleBirthYear[i]
+        if (age > oldestAge) {
+            oldestPersonIndex = i
+            oldestAge = age
+        }
+    }
+
+    return people[oldestPersonIndex]
 };
 
 // Do not edit below this line
